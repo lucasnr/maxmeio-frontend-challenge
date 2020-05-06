e0acdf49c50c1d9d004c49bb6edb75d77481b525
Lucas do Nascimento <lucasnascimentoribeiro13@gmail.com>
2020-05-06 15:13:28 -0300

    Add gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const { src, dest, series } = require('gulp'),
+  clean = require('gulp-clean'),
+  cssmin = require('gulp-cssmin'),
+  usemin = require('gulp-usemin'),
+  htmlmin = require('gulp-htmlmin');
+
+function cleanDist() {
+  return src('dist/', { read: false, allowEmpty: true }).pipe(clean());
+}
+
+function copyDist() {
+  return src(['src/**/*', '!src/assets/css/**']).pipe(dest('dist/'));
+}
+
+function buildHtml() {
+  return src('src/**/*.html')
+    .pipe(
+      usemin({
+        css: [cssmin],
+      })
+    )
+    .pipe(dest('dist/'));
+}
+
+function minifyHtml() {
+  return src('dist/**/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(dest('dist/'));
+}
+
+exports.default = series(cleanDist, copyDist, buildHtml, minifyHtml);
